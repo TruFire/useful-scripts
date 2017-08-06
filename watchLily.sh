00bdd31ee0cbe0c20d7928ec7687f56588853a41
Trudy Firestone <trudyfirestone@gmail.com>
2017-08-06 13:10:15 -0600

    Require an argument for watchLily.sh

--- watchLily.sh
+++ watchLily.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
-while true
-do
-	inotifywait "$1"
-	lilypond "$1"
-	if [ -z "$(pgrep evince)" ]
-	then
-		evince "${1%.ly}.pdf" &
-	fi
-done
-
+if [ -n "$1" ]
+then
+	while true
+	do
+		inotifywait "$1"
+		lilypond "$1"
+		if [ -z "$(pgrep evince)" ]
+		then
+			evince "${1%.ly}.pdf" &
+		fi
+	done
+else
+	echo "Remember to specify a file name."
+fi
