2e0438955492a5dea3808528fa81fe62b776de20
Trudy Firestone <trudyfirestone@gmail.com>
2017-08-06 12:29:19 -0600

    Made lilypond script auto open evince if it's not open.

--- watchLily.sh
+++ watchLily.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
-
-while true; do inotifywait $1; lilypond $1; done
+while true
+do
+	inotifywait "$1"
+	lilypond "$1"
+	if [ -z "$(pgrep evince)" ]
+	then
+		evince "${1%.ly}.pdf" &
+	fi
+done
 
